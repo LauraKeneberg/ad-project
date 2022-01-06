fd7f9d5ce875a4193aa18bba102c491095277602
LauraKeneberg <Laura.keneberg@gmail.com>
2022-01-06 17:16:19 +0300

    Оформление страницы списка объявлений

--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -1,9 +1,42 @@
 <template>
   <v-container>
 		<v-layout row> 
-      <v-flex xs12>
-				<h1>Ad List</h1> 
+			<v-flex xs12 sm6 offset-sm3>
+				<h1 class="text--secondary mb-3 mt-3">My ads</h1> 
 			</v-flex>
+			<v-card 
+				class="elevation-10 mb-5"
+				v-for="ad in ads"
+				:key="ad.id"
+			>
+				<v-layout row>
+					<v-flex xs4 class="pl-3">
+						<v-img
+						:src="ad.src"
+						height="175px"
+						></v-img>
+						Заголовок
+						<h2 class="text--primary">{{}}</h2>
+						и описание товара
+						<p>{{ }}</p>
+					</v-flex>
+
+					<v-flex xs8 class="pr-3">
+						<v-card-text>
+							<h2 class="text--primary">{{ad.title}}</h2>
+							<p>{{ad.desc}}</p>
+						</v-card-text>
+						<v-card-actions>
+							<v-spacer></v-spacer>
+							<v-btn color="primary"
+							:to="'/ad/' + ad.id">
+								Open
+							</v-btn>
+						</v-card-actions>	
+					</v-flex>
+				</v-layout>
+			</v-card>
+
 		</v-layout> 
 	</v-container>
 
@@ -13,6 +46,11 @@
 export default {
 	data () { 
 		return {
+			title: "...",
+			desc:"...",
+			promo: true,
+			src: "...",
+			id:""
 		} 	
 	}
 } 
