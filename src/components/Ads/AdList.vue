9a781fdd889d9fdd5e7a31a1600ea366ef305c27
LauraKeneberg <Laura.keneberg@gmail.com>
2022-01-06 17:32:38 +0300

    Оформление страницы списка объявлений

--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -1,10 +1,9 @@
 <template>
-  <v-container>
+	<v-container>
 		<v-layout row> 
 			<v-flex xs12 sm6 offset-sm3>
 				<h1 class="text--secondary mb-3 mt-3">My ads</h1> 
-			</v-flex>
-			<v-card 
+				<v-card 
 				class="elevation-10 mb-5"
 				v-for="ad in ads"
 				:key="ad.id"
@@ -15,12 +14,10 @@
 						:src="ad.src"
 						height="175px"
 						></v-img>
-						Заголовок
 						<h2 class="text--primary">{{}}</h2>
-						и описание товара
 						<p>{{ }}</p>
 					</v-flex>
-
+					
 					<v-flex xs8 class="pr-3">
 						<v-card-text>
 							<h2 class="text--primary">{{ad.title}}</h2>
@@ -35,8 +32,8 @@
 						</v-card-actions>	
 					</v-flex>
 				</v-layout>
-			</v-card>
-
+				</v-card>
+			</v-flex>
 		</v-layout> 
 	</v-container>
 
@@ -46,12 +43,37 @@
 export default {
 	data () { 
 		return {
-			title: "...",
-			desc:"...",
-			promo: true,
-			src: "...",
-			id:""
-		} 	
+			ads:[
+			{
+				title:"First",
+				desc:"Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+				promo: true,
+				src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+				id:"1"
+			},
+			{
+				title:"Second",
+				desc:"Second Desc",
+				promo: true,
+				src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+				id:"2"
+			},
+			{
+				title:"Third",
+				desc:"Thitd Desc",
+				promo: true,
+				src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+				id:"3"
+			},
+			{
+				title:"Fouth",
+				desc:"Fouth Desc",
+				promo: true,
+				src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+				id:"4"
+			}
+			]
+		}	
 	}
 } 
 </script>
